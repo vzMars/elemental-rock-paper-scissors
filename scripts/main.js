f2345b3f895784a0e229ca17b18a13678b705414
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2021-09-10 19:18:13 -0400

    updated the logic for the game to work with the ui that i created

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,28 @@
-let randomNumber;
-let computerSelection;
-let playerSelection;
-let computerScore = 0;
+let rivalScore = 0;
 let playerScore = 0;
-let round = 0;
+let battle = 0;
 let winner = false;
-//game();
+let battleDescription;
 
-function computerPlay() {
+const choices = document.querySelectorAll(".choice");
+const battleNumber = document.querySelector("#battleNumber");
+const player = document.querySelector("#playerScore");
+const rival = document.querySelector("#rivalScore");
+const battleText = document.querySelector("#battleDescription");
+const playAgain = document.querySelector("#playAgain");
+const playerPokemon = document.querySelector("#pokemon-player");
+const playerBG = document.querySelector("#playerBG");
+const rivalPokemon = document.querySelector("#pokemon-rival");
+const rivalBG = document.querySelector("#rivalBG");
+
+playAgain.addEventListener("click", resetUI);
+
+choices.forEach((choice) => choice.addEventListener("click", playBattle));
+playAgain.style.display = "none";
+
+function rivalChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  //console.log(randomNumber);
   if (randomNumber === 1) {
     return "Fire";
   } else if (randomNumber === 2) {
@@ -17,73 +32,103 @@ function computerPlay() {
   }
 }
 
-function playerPlay() {
-  let tempSelection = capitalize(prompt("Fire, Grass or Water?"));
-  console.log(`Temp Selection is ${tempSelection}`);
-  if (
-    tempSelection === "Fire" ||
-    tempSelection === "Grass" ||
-    tempSelection === "Water"
-  ) {
-    return tempSelection;
+function updateUI() {
+  battleNumber.textContent = `Battle: ${battle}`;
+  player.textContent = `Player Score: ${playerScore}`;
+  rival.textContent = `Rival Score: ${rivalScore}`;
+  battleText.textContent = `${battleDescription}`;
+  if (winner) {
+    playAgain.style.display = "inline-block";
+  }
+}
+
+function updateBattleContainer(image, selection, bgSelection) {
+  if (selection === "Fire") {
+    image.src = "/images/2.png";
+    bgSelection.style.backgroundColor = "var(--red)";
+  } else if (selection === "Grass") {
+    image.src = "/images/1.png";
+    bgSelection.style.backgroundColor = "var(--green)";
   } else {
-    console.log("Wrong choice pick only fire, grass or water!");
-    return playerPlay();
+    image.src = "/images/3.png";
+    bgSelection.style.backgroundColor = "var(--blue)";
   }
 }
 
-function capitalize(str) {
-  if (str !== null && str !== "") {
-    let str2 = str.toLowerCase();
-    return str2.charAt(0).toUpperCase() + str2.slice(1);
-  }
+function resetUI() {
+  winner = false;
+  rivalScore = 0;
+  playerScore = 0;
+  battle = 0;
+  battleDescription = "Red vs Blue";
+  playerPokemon.src = "/images/hidden1.png";
+  rivalPokemon.src = "/images/hidden2.png";
+  updateUI();
+  playAgain.style.display = "none";
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "Grass" && computerSelection === "Fire") {
-    computerScore++;
-    return "You Lose! Fire beats Grass";
-  } else if (playerSelection === "Water" && computerSelection === "Grass") {
-    computerScore++;
-    return "You Lose! Grass beats Water";
-  } else if (playerSelection === "Fire" && computerSelection === "Water") {
-    computerScore++;
-    return "You Lose! Water beats Fire";
-  } else if (playerSelection === computerSelection) {
-    return `It's a tie! ${playerSelection} is equal to ${computerSelection}`;
-  } else {
-    playerScore++;
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
-  }
-}
-
-function game() {
-  while (!winner) {
-    if (playerScore === 5 || computerScore === 5) {
-      if (playerScore > computerScore) {
-        console.log("Player Wins!");
-      } else {
-        console.log("Computer Wins!");
-      }
-      winner = true;
+function playBattle(e) {
+  //console.log("inside the playBattle method");
+  const playerSelection = e.target.dataset.choice;
+  const rivalSelection = rivalChoice();
+  if (!winner) {
+    if (
+      (playerSelection === "Grass" && rivalSelection === "Fire") ||
+      (playerSelection === "Water" && rivalSelection === "Grass") ||
+      (playerSelection === "Fire" && rivalSelection === "Water")
+    ) {
+      battleLost(playerSelection, rivalSelection);
+    } else if (playerSelection === rivalSelection) {
+      battleTie(playerSelection, rivalSelection);
     } else {
-      round++;
-      console.log(`Round ${round}`);
-      roundSetup();
-      console.log(playRound(playerSelection, computerSelection));
-      console.log(`Player: ${playerScore} Computer: ${computerScore}`);
+      battleWon(playerSelection, rivalSelection);
     }
   }
+  //console.log("end of the playBattle method");
 }
 
-function roundSetup() {
-  //computer choice
-  randomNumber = Math.floor(Math.random() * 3) + 1;
-  console.log(randomNumber);
-  computerSelection = computerPlay();
-  console.log(`Computer Selection is ${computerSelection}`);
+function battleWon(playerSelection, rivalSelection) {
+  //console.log("inside the battleWon method");
+  playerScore++;
+  battle++;
+  battleDescription = `You Win! ${playerSelection} is super effective against ${rivalSelection}`;
+  checkWinner();
+  updateBattleContainer(playerPokemon, playerSelection, playerBG);
+  updateBattleContainer(rivalPokemon, rivalSelection, rivalBG);
+  updateUI();
+  //console.log("end of the battleWon method");
+}
 
-  //player choice
-  playerSelection = playerPlay();
-  console.log(`Player Selection is ${playerSelection}`);
+function battleLost(playerSelection, rivalSelection) {
+  //console.log("inside the battleLost method");
+  rivalScore++;
+  battle++;
+  battleDescription = `You Lose! ${rivalSelection} is super effective against ${playerSelection}`;
+  checkWinner();
+  updateBattleContainer(playerPokemon, playerSelection, playerBG);
+  updateBattleContainer(rivalPokemon, rivalSelection, rivalBG);
+  updateUI();
+  //console.log("end of the battleLost method");
+}
+
+function battleTie(playerSelection, rivalSelection) {
+  //console.log("inside the battleTie method");
+  battle++;
+  battleDescription = `It's a tie! ${playerSelection} is equal to ${rivalSelection}`;
+  updateBattleContainer(playerPokemon, playerSelection, playerBG);
+  updateBattleContainer(rivalPokemon, rivalSelection, rivalBG);
+  updateUI();
+  //console.log("end of the battleTie method");
+}
+
+function checkWinner() {
+  //console.log("inside the checkwinner method");
+  if (playerScore === 5) {
+    winner = true;
+    battleDescription = `Red Wins!`;
+  } else if (rivalScore === 5) {
+    winner = true;
+    battleDescription = `Blue Wins!`;
+  }
+  //console.log("end of the checkWinner method");
 }
